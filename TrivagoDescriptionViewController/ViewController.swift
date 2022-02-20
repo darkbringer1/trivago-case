36cde307050c552b9aab83f7fc9908d60fff63d1
Dogukaan <dogukaan.kilicarslan@hepsiburada.com>
2022-02-20 11:35:51 +0300

    Initial Commit

---
+++ TrivagoDescriptionViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrivagoDescriptionViewController
+//
+//  Created by Dogukaan Kılıçarslan on 20.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
